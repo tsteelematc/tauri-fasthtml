c4f1d28eda8b3e28d58cf0797e437663efc11e91
tsteelematc <tsteele@madisoncollege.edu>
2026-03-25 09:55:40 -0500

    Very nice...
    And I like having...
    
    <branch>.chat.json
    
    history...
    
    So
    
    window-state-persistence.chat.json
    
    for this feature ! ! !

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,6 +9,7 @@ pub fn run() {
 
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_window_state::Builder::default().build())
         .setup(move |app| {
             let app_handle = app.handle().clone();
             let pid_store = python_pid_clone.clone();
